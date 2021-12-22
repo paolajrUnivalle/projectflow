b66cdc40c43304522d09dd78a59bac96927f847f
Paola Rodriguez <paola.rodriguez@correounivalle.edu.co>
2021-12-21 20:16:03 -0500

    primer commit todo ok

---
+++ src/myProject/OtraClase.java
@@ -0,0 +1,24 @@
+package myProject;
+
+public class OtraClase {
+    private int val;
+
+    public OtraClase(int val) {
+        this.val = val;
+    }
+
+    public int getVal() {
+        return val;
+    }
+
+    public void setVal(int val) {
+        this.val = val;
+    }
+
+    @Override
+    public String toString() {
+        return "OtraClase{" +
+                "val=" + val +
+                '}';
+    }
+}
